36c20d98a07a15874f027e60ee4141ca60ce039e
viniciusgpadilha <vp2002@gmail.com>
2023-06-01 10:21:34 -0300

    criacao dos seeders para as tabelas

---
+++ database/seeds/TenantsTableSeeder.php
@@ -0,0 +1,27 @@
+<?php
+
+use App\Models\{
+    Plan,
+    Tenant
+};
+use Illuminate\Database\Seeder;
+
+class TenantsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $plan = Plan::first();
+
+        $plan->tenants()->create([
+            'cnpj' => '23882706000120',
+            'name' => 'Cozinha de Casa',
+            'url' => 'cozinhadecasa',
+            'email' => 'cozinhadecasa@cozinhadecasa.com.br'
+        ]);
+    }
+}
--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -1,6 +1,7 @@
 <?php
 
-use App\User;
+use App\Models\User;
+use App\Models\Tenant;
 use Illuminate\Database\Seeder;
 
 class UsersTableSeeder extends Seeder
@@ -12,7 +13,9 @@ class UsersTableSeeder extends Seeder
      */
     public function run()
     {
-        User::create([
+        $tenant = Tenant::first();
+
+        $tenant->users()->create([
             'name' => 'Felipe',
             'email' => 'felipe@f.com.br',
             'password' => bcrypt('123456'),
