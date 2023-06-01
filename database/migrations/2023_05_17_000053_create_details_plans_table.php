070bd1c6796006e657c49fe76a98d0d92cc50173
viniciusgpadilha <vp2002@gmail.com>
2023-06-01 09:47:04 -0300

    inclusao tenant e ajuste nos arquivos de migration users e details_plan

--- database/migrations/2023_05_17_000053_create_details_plans_table.php
+++ database/migrations/2023_05_17_000053_create_details_plans_table.php
@@ -33,6 +33,6 @@ class CreateDetailsPlansTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('details_plans');
+        Schema::dropIfExists('details_plan');
     }
 }
