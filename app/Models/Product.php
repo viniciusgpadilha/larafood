9f6e905c1fc5da16d043e31281739ff634256adc
viniciusgpadilha <vp2002@gmail.com>
2023-06-07 16:11:42 -0300

    att

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -3,8 +3,15 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use App\Tenant\Traits\TenantTrait;
 
 class Product extends Model
 {
-    //
+    use TenantTrait;
+    
+    protected $fillable = ['title', 'flag', 'price', 'description', 'image'];
+
+    public function categories() {
+        $this->belongsToMany(Category::class);
+    }
 }
--- database/migrations/2023_06_07_140313_create_products_table.php
+++ database/migrations/2023_06_07_140313_create_products_table.php
@@ -15,11 +15,34 @@ class CreateProductsTable extends Migration
     {
         Schema::create('products', function (Blueprint $table) {
             $table->bigIncrements('id');
+            $table->unsignedBigInteger('tenant_id');
             $table->string('title')->unique();
             $table->string('flag')->unique();
+            $table->string('image');
             $table->double('price', 10, 2);
             $table->text('description');
             $table->timestamps();
+
+            $table->foreign('tenant_id')
+                        ->references('id')
+                        ->on('tenants')
+                        ->onDelete('cascade');
+        });
+
+        Schema::create('category_product', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->unsignedBigInteger('category_id');
+            $table->unsignedBigInteger('product_id');
+
+            $table->foreign('category_id')
+                        ->references('id')
+                        ->on('categories')
+                        ->onDelete('cascade');
+            
+            $table->foreign('product_id')
+                        ->references('id')
+                        ->on('products')
+                        ->onDelete('cascade');
         });
     }
 
