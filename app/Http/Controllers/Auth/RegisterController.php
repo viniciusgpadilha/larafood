48c3cbca353c18ae6e760c30fb7eef38464dda02
viniciusgpadilha <vp2002@gmail.com>
2023-08-18 10:39:12 -0300

    evento para vincular cargo ao usuario auto

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Controller;
 use App\Providers\RouteServiceProvider;
 use App\Services\TenantService;
 use App\Models\User;
+use App\Tenant\Events\TenantCreated;
 use Illuminate\Foundation\Auth\RegistersUsers;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Validator;
@@ -77,6 +78,8 @@ class RegisterController extends Controller
         $tenantService = app(TenantService::class);
 
         $user = $tenantService->make($plan, $data);
+
+        event(new TenantCreated($user));
 
         return $user;
     }
