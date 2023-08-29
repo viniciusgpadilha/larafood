e07cbb1e28d434fad8dd0323dd7f7228b9ba6e83
viniciusgpadilha <vp2002@gmail.com>
2023-08-29 13:51:47 -0300

    att product module

--- app/Http/Resources/ProductResource.php
+++ app/Http/Resources/ProductResource.php
@@ -15,8 +15,7 @@ class ProductResource extends JsonResource
     public function toArray($request)
     {
         return [
-            // 'tenant_id' => $this->tenant_id,
-            'flag' => $this->flag,
+            'identify' => $this->uuid,
             'title' => $this->title,
             'image' => url("storage/{$this->image}"),
             'price' => $this->price,
--- app/Repositories/Contracts/ProductRepositoryInterface.php
+++ app/Repositories/Contracts/ProductRepositoryInterface.php
@@ -5,5 +5,5 @@ namespace App\Repositories\Contracts;
 interface ProductRepositoryInterface
 {
     public function getproductsByTenantId(int $tenant_id, array $categories);
-    public function getProductByFlag(string $flag);
+    public function getProductByUuid(string $uuid);
 }
