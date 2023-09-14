f20a974b9d1b897be875e746a817e4a49650202b
viniciusgpadilha <vp2002@gmail.com>
2023-09-14 16:12:32 -0300

    criacao modulo avaliacao

---
+++ app/Models/Evaluation.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Evaluation extends Model
+{
+    protected $table = 'order_evaluations';
+
+    public function order()
+    {
+        return $this->belongsTo(Order::class);
+    }
+}
