00a0a151528bfe7fae57e63719d57ae30fd58a5c
viniciusgpadilha <vp2002@gmail.com>
2023-12-22 20:14:17 -0300

    att

--- resources/views/admin/pages/plans/edit.blade.php
+++ resources/views/admin/pages/plans/edit.blade.php
@@ -3,7 +3,7 @@
 @section('title', "Editar Plano {$plan->name}")
 
 @section('content_header')
-    <h1>Editar {{ $plan->name }}</h1>
+    <h1>Editar o {{ $plan->name }}</h1>
 @stop
 
 @section('content')
